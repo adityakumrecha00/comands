0dc6a9e7950cfad1058d97414af439ef7984378b
adityakumrecha00 <91272785+adityakumrecha00@users.noreply.github.com>
2025-04-26 14:13:26 +0530

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,20 +1,7 @@
-#!/bin/bash
-
-# 1. Update and Upgrade
-sudo apt update -y && sudo apt upgrade -y
-
-# 2. Install Essential Packages
-sudo apt install -y htop ca-certificates zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev tmux iptables curl nvme-cli git wget make jq libleveldb-dev build-essential pkg-config ncdu tar clang bsdmainutils lsb-release libssl-dev libreadline-dev libffi-dev gcc screen file unzip lz4
-
-# 3. Install NVM and Node.js (LTS)
-curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
-source ~/.bashrc
-nvm install --lts
-
-# 4. Install Python, Yarn, and Git
-sudo apt update
-sudo apt install -y python3 python3-venv python3-pip git curl
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update
-sudo apt install -y yarn
+git clone https://github.com/gensyn-ai/rl-swarm.git && cd rl-swarm
+cd modal-login
+yarn upgrade
+yarn add next@latest
+yarn add viem@latest
+cd ..
+screen -S gensynrlswarm
