5d0ebdc5f721a05ea611056e14a562f14e9906af
adityakumrecha00 <91272785+adityakumrecha00@users.noreply.github.com>
2025-04-26 08:14:05 +0530

    Create install_packages.sh

---
+++ install_packages.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt update
+sudo apt install htop ca-certificates zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev tmux iptables curl nvme-cli git wget make jq libleveldb-dev build-essential pkg-config ncdu tar clang bsdmainutils lsb-release libssl-dev libreadline-dev libffi-dev gcc screen file unzip lz4 -y
