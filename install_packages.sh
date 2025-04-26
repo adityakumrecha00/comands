a6a94238e06296a859372d4af94aeaee6b6a4645
adityakumrecha00 <91272785+adityakumrecha00@users.noreply.github.com>
2025-04-26 08:22:46 +0530

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,2 +1,1 @@
-curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
-source ~/.bashrc
+sudo apt install htop ca-certificates zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev tmux iptables curl nvme-cli git wget make jq libleveldb-dev build-essential pkg-config ncdu tar clang bsdmainutils lsb-release libssl-dev libreadline-dev libffi-dev jq gcc screen file unzip lz4 -y
