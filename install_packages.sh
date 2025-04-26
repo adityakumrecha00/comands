7e40aab8660810280dcc3eb34a26dfaf7f16c50d
adityakumrecha00 <91272785+adityakumrecha00@users.noreply.github.com>
2025-04-26 15:16:42 +0530

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -2,8 +2,8 @@
 
 # Check if the script is running in an interactive shell
 if [[ $- != *i* ]]; then
-  return
+  exit 0
 fi
 
 # Add to ~/.bashrc if necessary
-echo -e "# ~/.bashrc: executed by bash(1) for non-login shells.\n\n# If not running interactively, don't do anything\ncase \$- in\n    *i*) ;;\n    *) return;;\nesac" >> ~/.bashrc
+echo -e "# ~/.bashrc: executed by bash(1) for non-login shells.\n\n# If not running interactively, don't do anything\ncase \$- in\n    *i*) ;;\n    *) exit;;\nesac" >> ~/.bashrc
